3341c73c1e646e99c13f018bea06998331795f9f
howard <howardfk@gmail.com>
2015-07-01 17:59:24 -0400

    change script to match the fit anlysis

--- scpt_chi_analys_train.m
+++ scpt_chi_analys_train.m
@@ -21,20 +21,45 @@ for i=1:datacount
     bandwidth(i) = max(y) - min(y); %bandwith(n) = nth trains bandwidth
     UT(i) = train(i).UT;
 
-    [coff_1, coff_2, inv_coff_1, inv_coff_2, chi_norm]= fit_anlysis(temp_click_shape)
-    list_coff_1(i,:) = coff_1'
-    list_coff_2(i,:) = coff_2'
-    list_inv_coff_1(i,:) = inv_coff_1'
-    list_inv_coff_2(i,:) = inv_coff_2'
-    list_chi_norm(i,:) = chi_norm'
+    %coff_1 2xn matrix: 1xn = reg first order, 2xn = 1/sqrt first order
+    %coff_2 2xn matrix: 1xn = reg second order, 2xn = 1/sqrt firt odrer
+    [coff_1, coff_2, resid1, resid2, chi_norm]= fit_anlysis(temp_click_shape);
+    list_coff_1(i,:,:) = coff_1';
+    list_coff_2(i,:,:) = coff_2';
+    list_chi_norm(i,:) = chi_norm';
+
+    %Making residual graphs
+    figure(1)
+    scatter()
+    set(gcf,'Visible','off')
+
 end 
 
-figure(1)
-hist(list_chi_norm(:,1))
-figure(2)
-hist(list_chi_norm(:,2))
-figure(3)
-hist(list_chi_norm(:,3))
-figure(4)
-hist(list_chi_norm(:,4))
+test1 = list_chi_norm(:,1);
+test2 = list_chi_norm(:,2); 
+test3 = list_chi_norm(:,3); 
+test4 = list_chi_norm(:,4); 
 
+binning = [0:0.010:0.35]
+fig1 = figure(1)
+h1=histogram(test1,binning)
+hold on
+h2=histogram(test2,binning)
+title('histogram of chi squred for each fit')
+legend([h1 h2],'linear','second order')
+xlabel('Ratio of Chi and number of points')
+ylabel('count')
+h1.FaceColor = [1 0 0]
+h2.FaceColor = [1 1 0]
+
+figure(2)
+h3=histogram(test3,binning)
+hold on
+h4=histogram(test4,binning)
+h3.FaceColor = [0 1 0]
+h4.FaceColor = [0 0 1]
+
+title('histogram of chi squred for each fit')
+legend([h3 h4],'linear inverse sqrt','secondorder inverse sqrt')
+xlabel('Ratio of Chi and number of points')
+ylabel('count')
