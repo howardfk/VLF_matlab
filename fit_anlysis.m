5f9b30853c446f874c2d644c4a6994400457452f
howard <howardfk@gmail.com>
2015-07-01 00:17:20 -0400

    restructred function, finished chi_normlized, finished fit_data, needs testing

--- fit_anlysis.m
+++ fit_anlysis.m
@@ -1,25 +1,58 @@
 %Funciont written for residual anlysis on continouse hissler data
 %
-%fitcoff(i,1) = linear coff  x
-%fitcoff(i,2) = squred coff x^2
-%fitcoff(n,3) = constent
+%coff_1(j) = linear coffi  array j = 2
+%coff_2(j) = squred polyfit array j = 3
+%yinv_coff_1(j) = linear coffi  array j = 2
+%yinv_coff_2(j) = squred polyfit array j = 3
+%
 %fitcoff nx3 matrix
 %xypoints nx2 matrix
+%chi_norm is a vector of chi/number_points for each fit
+%1=first order
+%2=seconed order
+%3=first order inverse sqrt
+%4=second order ivnerse sqrt
 %
+%Equation for save keeping later delate if forgoten
+%resid = calc_residual(fitcoff(2),fitcoff(1),fitcoff(3), xypoints(:,1), xypoints(:,2));
 function [resid] = fit_anlysis(xypoints)
-    xypoints(:,1) = xypoints(:,1) - min(xypoints(:,1));
-    resid = calc_residual(fitcoff(2),fitcoff(1),fitcoff(3), xypoints(:,1), xypoints(:,2));
+    [coff_1, coff_2, inv_coff_1, inv_coff2] = fitdata(xypoints);
 end
 
-function [fitcoff] = fitdata(xypoints)
+function [coff_1, coff_2, inv_coff_1, inv_coff_2, chi_norm] = fitdata(xypoints)
+    yinv = (xypoints(:,2)).^(-0.5);
+    y = xypoints(:,2);
+    x = xypoints(:,1);
 
+    coff_1 = polyfit(x,y,1);
+    coff_2 =  polyfit(x,y,2);
+    yinv_coff_1 = polyfit(x,yinv,1);
+    yinv_coff_2 = polyfit(x,yinv,2);
+
+    %Chi value for first order polyfit
+    expt = polyval(coff_1,x)
+    chi_norm(1) = chai_sq(x,expt)
+    %Chi value for second order order polyfit
+    expt = polyval(coff_2,x)
+    chi_norm(2) = chai_sq(x,expt)
+    %Chi value for first oder order order polyfit inverse sqrt function
+    expt = polyval(yinv_coff_1,x)
+    chi_norm(3) = chai_sq(x,expt)
+    %Chi value for first oder order order polyfit inverse sqrt function
+    expt = polyval(yinv_coff_2,x)
+    chi_norm(4) = chai_sq(x,expt)
 end
 
-function [] = chi_sq(obs,expt)
+function [chi_norm] = chi_sq(obs,expt)
     if length(obs)~=length(expt)
         error('obsserved array not the same length as expected array for chi squared')
     else
         n = length(obs)
+        resid = obs-expt
+        std_sq = std(obs)^2
+        chi = (resid*resid')/std_sq
+        chi_norm = chi/n
+    end
 end
 
 function [resid]=calc_residual(coffsq,cofflin, coffc,x,y)
