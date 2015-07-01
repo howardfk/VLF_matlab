174ea8492e1f96af484b2eea2acba9b891867a87
howard <howardfk@gmail.com>
2015-07-01 17:36:38 -0400

    restructred output varibles slightly

--- fit_anlysis.m
+++ fit_anlysis.m
@@ -15,12 +15,17 @@
 %
 %Equation for save keeping later delate if forgoten
 %resid = calc_residual(fitcoff(2),fitcoff(1),fitcoff(3), xypoints(:,1), xypoints(:,2));
-function [coff_1, coff_2, inv_coff_1, inv_coff_2, chi_arr] = fit_anlysis(xypoints)
-    [coff_1, coff_2, inv_coff_1, inv_coff_2, chi_arr] = fitdata(xypoints);
-
+function [coff_firstorder, coff_secondorder, resid1, resid2, chi_arr] = fit_anlysis(xypoints)
+    [coff_1, coff_2, inv_coff_1, inv_coff_2, chi_arr, resid1, resid2] = fitdata(xypoints);
+    % 1 is the linear
+    % 2 is the linear inv sqrt
+    coff_firstorder(1,:) = coff_1;
+    coff_firstorder(2,:) = inv_coff_1;
+    coff_secondorder(1,:) = coff_2;
+    coff_secondorder(2,:) = inv_coff_2;
 end
 
-function [coff_1, coff_2, inv_coff_1, inv_coff_2, chi_arr] = fitdata(xypoints)
+function [coff_1, coff_2, inv_coff_1, inv_coff_2, chi_arr, resid1, resid2] = fitdata(xypoints)
     yinv = (xypoints(:,2)).^(-0.5);
     y = xypoints(:,2);
     x = xypoints(:,1);
@@ -30,10 +35,11 @@ function [coff_1, coff_2, inv_coff_1, inv_coff_2, chi_arr] = fitdata(xypoints)
     inv_coff_1 = polyfit(x,yinv,1);
     inv_coff_2 = polyfit(x,yinv,2);
 
-    resid1 = y - polyval(coff_1); 
-    resid2 = y - polyval(coff_2); 
-    resid1_inv = yinv - polyval(inv_coff_1); 
-    resid2_inv = yinv - polyval(inv_coff_2); 
+    % residn gives you the nth order fit col-1 gives you 'regulre' values col-2 gives you inv sqrt y
+    resid1(1,:) = y - polyval(coff_1); 
+    resid1(2,:)= yinv - polyval(inv_coff_1); 
+    resid2(1,:) = y - polyval(coff_2); 
+    resid2(2,:) = yinv - polyval(inv_coff_2); 
 
     %Chi value for first order polyfit
     expt = polyval(coff_1,x);
